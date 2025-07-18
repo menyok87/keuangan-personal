1fd25be85c3158f104eada4e738b17abe64f5516
mnx family <39312987+menyok87@users.noreply.github.com>
2025-07-18 23:41:27 +0700

    Fix React Native Android Setup Issues

---
+++ android/react-native.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/'],
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        android: {
+          sourceDir: '../node_modules/react-native-vector-icons/android',
+          packageImportPath: 'import io.github.react_native_vector_icons.VectorIconsPackage;',
+        },
+      },
+    },
+  },
+};
